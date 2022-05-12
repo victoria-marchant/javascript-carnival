71b106e0009423a7b63ff3e094be8db47b269664
Victoria Marchant <victoria@Victorias-MacBook-Pro.local>
2022-05-12 16:58:34 +1200

    Add unicorn task

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,32 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+let unicorns = document.getElementsByClassName('inflate-an-image')
+console.log(unicorns)
+
+for (let i = 0; i < unicorns.length; i++) {
+  unicorns[i].onclick = cellClicked
+}
+
+let noughtsTurn = true
+let gameIsOver
+
+function cellClicked(e) {
+  let unicorn = e.target
+  console.log(unicorn.src)
+  console.log('i clicked on: ' + unicorns)
+
+  if (unicorn.src.match('./images/unicorn-0.png')) {
+    unicorn.src = './images/unicorn-1.png'
+  } else if (unicorn.src.match('./images/unicorn-1.png')) {
+    unicorn.src = './images/unicorn-2.png'
+  } else if (unicorn.src.match('./images/unicorn-2.png')) {
+    unicorn.src = './images/unicorn-3.png'
+  } else if (unicorn.src.match('./images/unicorn-3.png')) {
+    unicorn.src = './images/unicorn-4.png'
+    return alert(
+      ` ${this.id}, the super rad unicorn says thanks a million for releasing my inner vermilion`
+    )
+  }
+}
